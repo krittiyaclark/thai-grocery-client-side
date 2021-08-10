9c2d6ea2e5b4f67f8228cef6a4b2764342d42ae9
Krittiya <krittiyaclark@gmail.com>
2021-08-09 20:34:41 -0400

    Set up front end

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -11,7 +11,7 @@ function getthaiGroceryBrand() {
           groceryBrand = document.querySelector('input').value;
           _context.prev = 1;
           _context.next = 4;
-          return regeneratorRuntime.awrap(fetch('http://localhost:8000/api/thai-grocery-product'));
+          return regeneratorRuntime.awrap(fetch("http://localhost:8000/api/thai-grocery-product/".concat(groceryBrand)));
 
         case 4:
           res = _context.sent;
@@ -21,18 +21,19 @@ function getthaiGroceryBrand() {
         case 7:
           data = _context.sent;
           console.log(data);
-          _context.next = 14;
+          document.querySelector('h2').innerText = data.brand;
+          _context.next = 15;
           break;
 
-        case 11:
-          _context.prev = 11;
+        case 12:
+          _context.prev = 12;
           _context.t0 = _context["catch"](1);
           console.log(_context.t0);
 
-        case 14:
+        case 15:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[1, 11]]);
+  }, null, null, [[1, 12]]);
 }
--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ document.querySelector('button').addEventListener('click', getthaiGroceryBrand)
 async function getthaiGroceryBrand() {
 	const groceryBrand = document.querySelector('input').value
 	try {
-		const res = await fetch('http://localhost:8000/api/thai-grocery-product')
+		const res = await fetch(
+			`http://localhost:8000/api/thai-grocery-product/${groceryBrand}`
+		)
 		const data = await res.json()
 
 		console.log(data)
+		document.querySelector('h2').innerText = data.brand
 	} catch (err) {
 		console.log(err)
 	}
